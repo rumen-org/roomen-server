3ed5d3d05151ff9e413746a6f1b05c4b5b1042ae
김인태 <dlsxody1@naver.com>
2024-03-12 16:05:13 +0900

    initial commit

---
+++ src/utils/swagger.util.ts
@@ -0,0 +1,17 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('DevMet')
+    .setDescription('DevMet API 명세')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', name: 'JWT', in: 'header' },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api-docs', app, document);
+}
